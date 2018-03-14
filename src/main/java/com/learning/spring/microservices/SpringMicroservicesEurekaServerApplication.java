37923f8551f5a1a16a55d7aafb4291407fad35d4
Rekha Rokkam <rekha.rokkam@yahoo.com>
2018-03-14 12:29:28 -0700

    Project starter Kit

---
+++ src/main/java/com/learning/spring/microservices/SpringMicroservicesEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.learning.spring.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringMicroservicesEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesEurekaServerApplication.class, args);
+	}
+}
